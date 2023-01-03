f7ba6a937643bc0f63ef7f996d96eaf69f660a31
talka19 <talka19.10@gmail.com>
2023-01-03 21:22:22 +0200

    finished tasks1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const itemsMarkup = onClickGallary(galleryItems);
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', itemsMarkup)
+galleryEl.addEventListener('click',onChangeItems)
+
+function onClickGallary(items) {
+  return items.map(({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`
+  }).join('')
+}
+
+
+function onChangeItems(event) {
+  event.preventDefault();
+   if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryEl.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+ 
+}
